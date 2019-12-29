73f1dd5e93c1c0db8cfe19a40f65eee70ff3e5f7
taohouqi <384520505@qq.com>
2019-12-29 08:57:08 +0800

    first commit

---
+++ public/precache-manifest.d79c0d08277ef21e5b7c5a16efd764b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b372c4e21efd98e520080bca3ad6e3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "585bd0d6d6d440133ed4",
+    "url": "/static/css/2.8f094c9d.chunk.css"
+  },
+  {
+    "revision": "272e79f03e679df07f61",
+    "url": "/static/css/main.442cca92.chunk.css"
+  },
+  {
+    "revision": "585bd0d6d6d440133ed4",
+    "url": "/static/js/2.b33c5f40.chunk.js"
+  },
+  {
+    "revision": "272e79f03e679df07f61",
+    "url": "/static/js/main.6c9d440b.chunk.js"
+  },
+  {
+    "revision": "d653c3e1ff0c4fc20192",
+    "url": "/static/js/runtime-main.d01d61ca.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ef2f27baa7f3e605657596fbef433435",
+    "url": "/static/media/bg.ef2f27ba.jpg"
+  }
+]);
